68a027e327ee1c396b4832974f5d36d6d8980a3e
PieterNatanael <pieternatanael@icloud.com>
2024-03-18 11:26:39 +0800

    Initial Commit

---
+++ Screen Time Break/Screen_Time_BreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_Time_BreakApp.swift
+//  Screen Time Break
+//
+//  Created by Pieter Yoshua Natanael on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Screen_Time_BreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
