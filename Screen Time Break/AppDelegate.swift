4fe3178567d4f3e74557ff4efff7d2a53c5c581f
PieterNatanael <pieternatanael@icloud.com>
2024-03-19 22:57:50 +0800

    Initial Commit

---
+++ Screen Time Break/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Screen Time Break
+//
+//  Created by Pieter Yoshua Natanael on 18/03/24.
+//
+
+import SwiftUI
+
+struct AppDelegate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDelegate()
+}
